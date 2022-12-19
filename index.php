12c8408f2d2babb18630c9ab7d0d93ecd5a92a72
Behnoosh Moshtagh <behnoosh.moshtagh@gmail.com>
2022-12-19 11:25:37 +0330

    Add Chatter

--- index.php
+++ index.php
@@ -1,11 +1,33 @@
 <?php
 
 require 'vendor/autoload.php';
+include 'bootstrap.php';
+
+use Chatter\Models\Message;
+use Chatter\Middleware\Logging as ChatterLogging;
 
 $app = new \Slim\App();
-// Define app routes
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    return $response->write("Hello " . $args['name']);
+$app->add(new ChatterLogging());
+
+
+$app->get('/messages', function ($request, $response, $args) {
+    $_message = new Message();
+
+    $messages = $_message->all();
+
+    $payload = [];
+    foreach($messages as $_msg) {
+        $payload[$_msg->id] = ['body' => $_msg->body, 'user_id' => $_msg->user_id, 'created_at' => $_msg->created_at];
+    }
+
+    return $response->withStatus(200)->withJson($payload);
+});
+
+
+
+
+$app->get('/', function ($request, $response, $args) {
+    return "This is a catch all route for the root that doesn't do anything useful.";
 });
 
 // Run app
